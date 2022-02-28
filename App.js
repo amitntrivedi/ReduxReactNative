133cc200b5dddb8a4c22b579c8be1af81637a3c9
Amit Kumar Trivedi <amitntrivedi@gmail.com>
2022-02-28 15:11:14 +0530

    Update App.js
    Added simple counter with useState hook.

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import type {Node} from 'react';
+import { useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,75 +17,28 @@ import {
   Text,
   useColorScheme,
   View,
+  Button
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [count,setCount ] =useState(10); 
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+    <SafeAreaView>
+      <ScrollView         contentInsetAdjustmentBehavior="automatic">
+         <View>
+          <Text style={styles.sectionTitle} >         Redux React Native Example </Text>   
+        </View> 
+        <View>
+          <Button title="Increment" onPress={()=>{setCount(count +1 )}} > </Button>   
+        </View> 
+        <View>
+          <Text>    {count}   </Text>   
+        </View> 
+        <View>
+          <Button title="Decrement" onPress={()=>{setCount(count -1 )}} > </Button>   
+        </View> 
+        
       </ScrollView>
     </SafeAreaView>
   );
